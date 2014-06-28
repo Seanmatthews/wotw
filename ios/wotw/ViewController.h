cb94f6e82b0ec781ace99d75611aa80574778efc
sean matthews <sean@seans-MacBook-Pro.local>
2014-06-28 16:50:54 -0400

    Directory restructure

---
+++ ios/wotw/ViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  wotw
+//
+//  Created by sean matthews on 6/28/14.
+//  Copyright (c) 2014 Rowboat Entertainment. All rights reserved.
+//
+
+@import UIKit;
+@import iAd;
+@import CoreLocation;
+@import MapKit;
+
+@interface ViewController : UIViewController <ADBannerViewDelegate, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
+
+@property (nonatomic, strong) IBOutlet MKMapView *mapView;
+@property (nonatomic, strong) IBOutlet UIView *wallView;
+@property (nonatomic, strong) IBOutlet UITextField *textField;
+
+- (IBAction)pressedMapButton:(id)sender;
+- (IBAction)pressedWallButton:(id)sender;
+
+@end
---
+++ ios/wotwTests/wotwTests.m
@@ -0,0 +1,34 @@
+//
+//  wotwTests.m
+//  wotwTests
+//
+//  Created by sean matthews on 6/28/14.
+//  Copyright (c) 2014 Rowboat Entertainment. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface wotwTests : XCTestCase
+
+@end
+
+@implementation wotwTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
